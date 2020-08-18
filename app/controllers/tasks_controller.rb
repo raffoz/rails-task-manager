74dd032583970fd01200cf49e1db0ace41241fff
Raffaele Viggiani <raffaele.viggiani@gmail.com>
2020-08-18 16:08:46 +0200

    Done tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,49 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    # no need for app/views/tasks/create.html.erb
+    redirect_to tasks_path
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  # def completed?(task)
+  #   task.completed
+  # end
+
+    private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+  @task = Task.find(params[:id])
+  end
+
+end
